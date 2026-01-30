6c471a19b72984f6fb36d5567e12b90e06831d18
Irénée <irenee.ajeneza@n8n.io>
2026-01-30 15:18:43 +0000

    feat: removed un-used dto

--- packages/@n8n/api-types/src/dto/secrets-provider/list-secrets-completions-query.dto.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-import { Z } from 'zod-class';
-
-import { booleanFromString } from '../../schemas/boolean-from-string';
-
-export class ListSecretsCompletionsQueryDto extends Z.class({
-	projectId: z.string().optional(),
-	includeGlobal: booleanFromString.optional(),
-}) {}
