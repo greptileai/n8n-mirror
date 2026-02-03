92f6b9ee2ef3baa867acb1e85ed206c25040fabb
Guillaume Jacquart <jacquart.guillaume@gmail.com>
2026-02-03 18:04:42 +0100

    feat(core): Add log streaming for personal publishing restriction changes

--- packages/cli/src/events/maps/relay.event-map.ts
+++ packages/cli/src/events/maps/relay.event-map.ts
@@ -678,8 +678,8 @@ export type RelayEventMap = {
 	// #region Instance Policies
 
 	'instance-policies-updated': {
-		userId: string;
-		settingName: '2fa_enforcement' | 'workflow_publishing';
+		user: UserLike;
+		settingName: '2fa_enforcement' | 'workflow_publishing' | 'workflow_sharing';
 		value: boolean;
 	};
 
